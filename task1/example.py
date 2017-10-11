51522141ca6e6cbe230c339a70846c2f83d85e0d
Calin Cruceru <ccruceru@student.ethz.ch>
2017-10-11 11:29:36 +0200

    Add several changes: 1. Use regular hash function (Python built-in) for bands. 2. I think that ass and bss should be ~ Unif(1/0, p) instead of Unif(1/0, N) (p - large prime number, N - number of shingles).  This did not affect too much the performance. 3. Yield the min hashes from the mapper as well, and compute the empiric similarity in the reducer, and only yield if that's above the target similarity.

--- task1/example.py
+++ task1/example.py
@@ -1,23 +1,33 @@
+from __future__ import division
+
 import numpy as np
 import random
 
-MAXIMUM_HASH_FNS = 1020
+TARGET_SIMILARITY = 0.85
+
+# We need to ensure that (1 / BANDS_COUNT) ^ (1 / ROWS_COUNT) is approximately
+# TARGET_SIMILARITY.
+MAX_HASH_FNS = 966
 LARGE_PRIME = 1000000007
 TOTAL_SHINGLES = 8193
 
-ROWS_COUNT = 30
-BANDS_COUNT = MAXIMUM_HASH_FNS / ROWS_COUNT
-BUCKETS_COUNT = 100000007 # Check later
+ROWS_COUNT = 23
+BANDS_COUNT = MAX_HASH_FNS // ROWS_COUNT
+BUCKETS_COUNT = 100000007 # Do we need this?
 
-ass = np.random.choice(range(1, TOTAL_SHINGLES), MAXIMUM_HASH_FNS)
-bss = np.random.choice(range(TOTAL_SHINGLES), MAXIMUM_HASH_FNS)
+# NOTE: I think there is a mistake in Krause's slides: it seems that
+# a,b ~ Unif(0/1, p), not Unif(0/1, shingles_count).
+# Anyway, this did not change much.
+ass = np.array([random.randint(1, LARGE_PRIME - 1)
+                for _ in range(MAX_HASH_FNS)])
+bss = np.array([random.randint(0, LARGE_PRIME - 1)
+                for _ in range(MAX_HASH_FNS)])
 
-band_ass = np.random.choice(range(1, TOTAL_SHINGLES), ROWS_COUNT)
-band_bss = np.random.choice(range(TOTAL_SHINGLES), ROWS_COUNT)
 
 def hash_vector(band):
-    return np.sum(((np.multiply(band, band_ass) + band_bss) % LARGE_PRIME) % \
-                BUCKETS_COUNT) % BUCKETS_COUNT
+    # We use regular hashes here.
+    # TODO: Why do we need the buckets?
+    return hash(band.data) % BUCKETS_COUNT
 
 
 def mapper(key, value):
@@ -30,10 +40,11 @@ def mapper(key, value):
     # Compute min hashes.
     min_hashes = np.min(((np.outer(shingles, ass) + bss) % LARGE_PRIME) %
                             TOTAL_SHINGLES, axis=0)
+    min_hashes.flags.writeable = False
 
     for b in range(BANDS_COUNT):
         bucket = hash_vector(min_hashes[b * ROWS_COUNT : (b + 1) * ROWS_COUNT])
-        yield (str(bucket) + '_' + str(b)), docid
+        yield (str(bucket) + '_' + str(b)), (docid, min_hashes)
 
 
 def reducer(key, values):
@@ -42,4 +53,12 @@ def reducer(key, values):
     values = sorted(values)
     for i in range(len(values)):
         for j in range(i + 1, len(values)):
-            yield values[i], values[j]
+            doc1, min_hahes1 = values[i]
+            doc2, min_hahes2 = values[j]
+            # NOTE: 6. Examine each candidate pair’s signatures and determine
+            # whether the fraction of components in which they agree is at
+            # least t (MMDS (p. 91)).
+            emipiric_similarity = np.sum(min_hahes1 == min_hahes2) / \
+                                    len(min_hahes1) # MAX_HASH_FNS
+            if emipiric_similarity > TARGET_SIMILARITY:
+                yield doc1, doc2
